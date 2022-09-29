e0d9d51ae98866b715292d078f26c20e362c2bf3
wuzheng <17173as@163.com>
2022-09-29 11:48:37 +0800

    网络编程-优雅关闭相关代码。

--- network/ch07/unixstreamserver.c
+++ network/ch07/unixstreamserver.c
@@ -23,6 +23,7 @@ int main(int argc ,char **argv) {
     }
 
 //    设置一个本地文件路径，创建了一个本地地址，这里的本地地址和 IPv4、IPv6 地址可以对应
+//  unlink 删除文件，以便把存在的文件删除掉，这样可以保持幂等性。
     unlink(local_path);
     bzero(&servaddr,sizeof(servaddr));
     servaddr.sun_family = AF_LOCAL;
@@ -44,6 +45,7 @@ int main(int argc ,char **argv) {
 
     char buf[BUFFER_SIZE];
 
+//    使用 read 和 write 函数从套接字中按照字节流的方式读取和发送数据。
     while (1) {
         bzero(buf, sizeof(buf));
         if (read(connfd, buf, BUFFER_SIZE) == 0) {
